f9fb99bdf52e4c6a662147003cd4e6a79b3220b9
erichongrac <148745337+erichongrac@users.noreply.github.com>
2024-03-11 14:20:25 +0800

    add redux thunk

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -2,9 +2,9 @@ import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, 
 import { Product } from "../../app/models/product";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import agent from "../../app/api/agent";
 import { LoadingButton } from "@mui/lab";
-import { useStoreContext } from "../../app/context/StoreContext";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { addBasketItemAsync } from "../basket/basketSlice";
 
 interface Props {
     product: Product;
@@ -12,15 +12,16 @@ interface Props {
 
 export default function ProductCard({ product }: Props) {
     const [loading, setLoading] = useState(false);
-    const { setBasket } = useStoreContext();
+    const { status } = useAppSelector(state => state.basket);
+    const dispatch = useAppDispatch();
 
-    function handleAddItem(productId: number) {
-        setLoading(true);
-        agent.Basket.addItem(productId)
-            .then(basket => setBasket(basket))
-            .catch(error => console.log(error))
-            .finally(() => setLoading(false));
-    }
+    // function handleAddItem(productId: number) {
+    //     setLoading(true);
+    //     agent.Basket.addItem(productId)
+    //         .then(basket => dispatch(setBasket(basket)))
+    //         .catch(error => console.log(error))
+    //         .finally(() => setLoading(false));
+    // }
 
     return (
         <Card sx={{ maxWidth: 345 }}>
@@ -49,7 +50,10 @@ export default function ProductCard({ product }: Props) {
                 </Typography>
             </CardContent>
             <CardActions>
-                <LoadingButton loading={loading} onClick={() => handleAddItem(product.id)} size="small">Add to cart</LoadingButton>
+                <LoadingButton loading={status.includes('pendingAddItem' + product.id)}
+                    onClick={() => dispatch(addBasketItemAsync({ productId: product.id }))}
+                    size="small">Add to cart
+                </LoadingButton>
                 <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
             </CardActions>
         </Card>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,7 +7,6 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router/Routes.tsx";
-import { StoreProvider } from "./app/context/StoreContext.tsx";
 import { Provider } from "react-redux";
 import { store } from "./app/store/configureStore.ts";
 
@@ -18,10 +17,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <StoreProvider>
       <Provider store={store}>
         <RouterProvider router={router} />
       </Provider>
-    </StoreProvider>
   </React.StrictMode>
 );
